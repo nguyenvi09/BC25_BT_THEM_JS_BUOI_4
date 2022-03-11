468b7302f86598012fc6ba26cff9914d73898d29
nguyenvi09 <nguyentanvi19942010@gmail.com>
2022-03-11 10:32:32 +0700

    upload source bt3

--- main.js
+++ main.js
@@ -254,4 +254,75 @@ document.getElementById("checkDay").onclick = function() {
     };
 };
 
+/**
+ * BT3:
+ * - Đầu vào: số nguyên có 3 chữ số
+ * 
+ * - Xử lý:
+ * B1: tạo biến so 
+ * B2: kiểm tra so đó có phải số nguyên có 3 chữ số hay không
+ * B3: tách hangTram / hangChuc / hangDonvi
+ * B4: dùng switch case để lưu giá trị cách đọc số. Nếu hangChuc = 0 và hangDonVi khác 0
+ * -> gán hangChuc = "lẻ";
+ * 
+ * - Đầu ra: in cách đọc số ra màn hình web
+ * 
+ */
+
+document.getElementById("cachDocSo").onclick = function() {
+    var so = document.getElementById("so").value;
+    
+    if( so >= 100 && so <= 999){
+        var hangTram = Math.floor(so / 100);
+        var hangChuc = Math.floor((so / 10) % 10);
+        var hangDonVi = Math.floor(so % 10);
+        
+        switch(hangTram){
+            case 1: hangTram = "Một trăm"; break;
+            case 2: hangTram = "Hai trăm"; break;
+            case 3: hangTram = "Ba trăm"; break;
+            case 4: hangTram = "Bốn trăm"; break;
+            case 5: hangTram = "Năm trăm"; break;
+            case 6: hangTram = "Sáu trăm"; break;
+            case 7: hangTram = "Bảy trăm"; break;
+            case 8: hangTram = "Tám trăm"; break;
+            case 9: hangTram = "Chín trăm"; break;
+        };
+    
+        if( hangChuc == 0 &&  hangDonVi != 0 ){
+            hangChuc = "lẻ";
+        }
+
+        switch(hangChuc){
+            case 0: hangChuc = ""; break; 
+            case 1: hangChuc = "mười"; break;
+            case 2: hangChuc = "hai mươi"; break;
+            case 3: hangChuc = "ba mươi"; break;
+            case 4: hangChuc = "bốn mươi"; break;
+            case 5: hangChuc = "năm mươi"; break;
+            case 6: hangChuc = "sáu mươi"; break;
+            case 7: hangChuc = "bảy mươi"; break;
+            case 8: hangChuc = "tám mươi"; break;
+            case 9: hangChuc = "chín mươi"; break;
+        };
+    
+
+        switch(hangDonVi){
+            case 0: hangDonVi = ""; break;
+            case 1: hangDonVi = "một"; break;
+            case 2: hangDonVi = "hai"; break;
+            case 3: hangDonVi = "ba"; break;
+            case 4: hangDonVi = "bốn"; break;
+            case 5: hangDonVi = "năm"; break;
+            case 6: hangDonVi = "sáu"; break;
+            case 7: hangDonVi = "bảy"; break;
+            case 8: hangDonVi = "tám"; break;
+            case 9: hangDonVi = "chín"; break;
+        };
+        document.getElementById("ketQuaCachDocSo").innerHTML = hangTram + " " + hangChuc + " " + hangDonVi;
+    };
+
+    
+}
+
 
